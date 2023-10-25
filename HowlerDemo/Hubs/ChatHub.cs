261c0fc63aa9ae15b5627984405619d50e8afbd4
Will Meister <wmeister@exele.com>
2023-10-25 15:25:07 -0400

    WIP Add non-working code to attempt to send messages.

--- HowlerDemo/Hubs/ChatHub.cs
+++ HowlerDemo/Hubs/ChatHub.cs
@@ -4,9 +4,9 @@ namespace SignalR.Messages
 {
     class MessageHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", message);
         }
     }
 }
---
+++ HowlerDemo/wwwroot/js/messages.js
@@ -0,0 +1,20 @@
+﻿import { signalR } from "./signalr/dist/browser/signalr"
+
+var connection = new signalR.HubConnectionBulder().withUrl("/messageHub").build(); //Create a connection object for the /messageHub endpoint
+
+document.getElementById("send-message-button").disabled = true;
+
+connection.on("ReceiveMessage", function (message) {
+    var li = document.createElement("li");
+    li.textContent = message
+    $("#messages-list").appendChild(li);
+});
+
+connection.start()
+document.getElementById("send-message-button").on("onClick", function () {
+    connection.on("click", function () {
+        var text = $("#message-text-input").textContent
+        connection.invoke("SendMessage", text)
+    })
+});
+
